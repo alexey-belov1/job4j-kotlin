85981455180f04bcc3634f3821b3a15327f4c624
alexey-belov1 <veshousis1@gmail.com>
2020-10-02 13:40:48 +0600

    3. Тестирование через KotlinTest

---
+++ src/test/kotlin/ru/job4j/base/CalcTest.kt
@@ -0,0 +1,22 @@
+package ru.job4j.base
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class CalcTest : StringSpec({
+    "1 + 1 = 2" {
+        add(1.0, 1.0) shouldBe 2.0
+    }
+
+    "3 - 1 = 2" {
+        subtract(3.0, 1.0) shouldBe 2.0
+    }
+
+    "2 * 3 = 6" {
+        multiply(2.0, 3.0) shouldBe 6.0
+    }
+
+    "6 / 2 = 3" {
+        div(6.0, 2.0) shouldBe 3.0
+    }
+})
