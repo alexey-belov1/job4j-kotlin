0144094f250a079855294aa4095d92d6fc0faf5e
alexey-belov1 <veshousis1@gmail.com>
2020-10-04 19:54:27 +0600

    5. Циклы while и for

---
+++ src/main/kotlin/ru/job4j/base/DrawX.kt
@@ -0,0 +1,21 @@
+package ru.job4j.base
+
+fun draw(size: Int): Boolean {
+    if (size <= 0 || size % 2 == 0) {
+        print("Wrong input")
+        return false
+    }
+    for(row in 0 until size) {
+        for(column in 0 until size) {
+            print(
+                    if (row == column || row == size - 1 - column) "x" else " "
+            )
+        }
+        println();
+    }
+    return true
+}
+
+fun main() {
+    draw(5)
+}
