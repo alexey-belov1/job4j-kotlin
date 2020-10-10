168b1ec5cc1b43171324971b5ee5be8ef70e6a43
alexey-belov1 <veshousis1@gmail.com>
2020-10-10 22:18:51 +0600

    2. Модель данных POJO

--- src/test/kotlin/ru/job4j/tracker/TrackerTest.kt
+++ src/test/kotlin/ru/job4j/tracker/TrackerTest.kt
@@ -8,15 +8,15 @@ import ru.job4j.tracker.Tracker
 class TrackerTest : StringSpec({
     "When add in tracker" {
         val tracker = Tracker()
-        val item = Item("Item")
+        val item = Item(name = "Item")
         tracker.add(item)
         tracker.findById(item.id)?.name shouldBe "Item"
     }
 
     "When findAll in tracker" {
         val tracker = Tracker()
-        val item1 = Item("Item1")
-        val item2 = Item("Item2")
+        val item1 = Item(name = "Item1")
+        val item2 = Item(name = "Item2")
         tracker.add(item1)
         tracker.add(item2)
         tracker.findAll() shouldBe arrayListOf(item1, item2)
@@ -24,9 +24,9 @@ class TrackerTest : StringSpec({
 
     "When findByName in Tracker" {
         val tracker = Tracker()
-        val item11 = Item("Item1")
-        val item12 = Item("Item1")
-        val item2 = Item("Item2")
+        val item11 = Item(name = "Item1")
+        val item12 = Item(name = "Item1")
+        val item2 = Item(name = "Item2")
         tracker.add(item11)
         tracker.add(item12)
         tracker.add(item2)
@@ -35,16 +35,16 @@ class TrackerTest : StringSpec({
 
     "When replace in Tracker and true" {
         val tracker = Tracker()
-        val item = Item("item")
+        val item = Item(name = "item")
         tracker.add(item)
-        val itemWithDesc = Item("Item with description")
+        val itemWithDesc = Item(name = "Item with description")
         tracker.replace(item.id, itemWithDesc) shouldBe true
         tracker.findById(item.id)?.name shouldBe "Item with description"
     }
 
     "When replace in Tracker and false" {
         val tracker = Tracker()
-        val item = Item("Item")
+        val item = Item(name = "Item")
         tracker.add(item)
         val itemWithDesc = Item("Item with description")
         tracker.replace("0", itemWithDesc) shouldBe false
@@ -53,7 +53,7 @@ class TrackerTest : StringSpec({
 
     "When delete in Tracker and true" {
         val tracker = Tracker()
-        val item = Item("Item")
+        val item = Item(name = "Item")
         tracker.add(item)
         tracker.delete(item.id) shouldBe true
         tracker.findById(item.id) shouldBe null
@@ -61,7 +61,7 @@ class TrackerTest : StringSpec({
 
     "When delete in Tracker and false" {
         val tracker = Tracker()
-        val item = Item("Item")
+        val item = Item(name = "Item")
         tracker.add(item)
         tracker.delete("") shouldBe false
         tracker.findById(item.id)?.name shouldBe "Item"
