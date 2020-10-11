4957e03a108648f755642ac301e49d3fe4ca7cb5
alexey-belov1 <veshousis1@gmail.com>
2020-10-11 17:15:03 +0600

    5. Класс-компаньон

--- src/main/kotlin/ru/job4j/tracker/StartUI.kt
+++ src/main/kotlin/ru/job4j/tracker/StartUI.kt
@@ -1,6 +1,6 @@
 package ru.job4j.tracker
 
-object StartUI {
+internal class StartUI {
 
     private val input = ConsoleInput()
     private val tracker = Tracker()
@@ -15,31 +15,33 @@ object StartUI {
                         + "2. Exit Program"
             )
             when (input.askInt("Select: ")) {
-                0 -> run = createAction()
-                1 -> run = showAction()
+                0 -> run = createAction(input, tracker)
+                1 -> run = showAction(tracker)
                 2 -> run = exitAction()
             }
         }
     }
 
-    private fun createAction() : Boolean {
-        val name = input.askStr("Enter name: ")
-        val item = Item(name = name)
-        tracker.add(item)
-        return true
-    }
-
-    private fun showAction() : Boolean {
-        val items = tracker.findAll()
-        for ((id, name) in items) {
-            println("$name $id")
+    companion object {
+        fun createAction(input : Input, tracker : ITracker) : Boolean {
+            val name = input.askStr("Enter name: ")
+            val item = Item(name = name)
+            tracker.add(item)
+            return true
         }
-        return true
-    }
 
-    private fun exitAction() : Boolean = false
+        fun showAction(tracker : ITracker) : Boolean {
+            val items = tracker.findAll()
+            for ((id, name) in items) {
+                println("$name $id")
+            }
+            return true
+        }
+
+        fun exitAction() : Boolean = false
+    }
 }
 
 fun main() {
-    StartUI.init()
+    StartUI().init()
 }
